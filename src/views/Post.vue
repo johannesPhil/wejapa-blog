b25f2c8ebb3dc3049c795e8602c9c7334952b58e
John Peter <jay3pee@gmail.com>
2020-08-15 13:58:05 +0100

    new comment displays

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -11,14 +11,43 @@
       <div class="form-container">
         <form action="" @submit.prevent="addComment">
           <div class="form__control">
-            <label for="comment" class="form__el">Add Comment</label>
+            <label for="name" class="form__el">
+              Your name
+              <i>(required)</i>
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              class="form__el"
+              required
+              v-model="newComment.name"
+            />
+            <label for="mail" class="form__el">
+              Your Mail
+              <i>(required)</i>
+              )
+            </label>
+            <input
+              type="email"
+              name="mail"
+              id="mail"
+              class="form__el"
+              required
+              v-model="newComment.email"
+            />
+            <label for="comment" class="form__el">
+              Add Comment
+              <i>(required)</i>
+            </label>
             <textarea
+              required
               name="comment"
               id="comment"
               cols="30"
               rows="10"
               class="form__el"
-              v-model="comment"
+              v-model="newComment.body"
             ></textarea>
           </div>
           <button type="submit" class="form__el">Add Comment</button>
@@ -45,11 +74,15 @@ export default {
 
       body: '',
 
-      comments: [],
+      comments: {},
 
       nullComment: false,
 
-      comment: '',
+      newComment: {
+        name: '',
+        email: '',
+        body: '',
+      },
     }
   },
 
@@ -101,18 +134,40 @@ export default {
     },
 
     addComment() {
-      fetch(
-        'https://jsonplaceholder.typicode.com/posts/' + this.id + '/comments',
-        {
-          method: 'POST',
-          body: JSON.stringify(this.comment),
-        },
-      ).then((response) => {
-        response.json()
-        if (response.status === 201) {
-          alert('Your comment has been added')
-        }
-      })
+      //Mimic adding a resource
+
+      if (this.newComment.email && this.newComment.body) {
+        console.log(this.newComment.mail)
+        console.log(this.newComment)
+
+        this.comments.push(this.newComment)
+      }
+      this.newComment = {
+        email: '',
+        body: '',
+      }
+
+      // fetch(
+      //   'https://jsonplaceholder.typicode.com/posts/' + this.id + '/comments',
+      //   {
+      //     method: 'POST',
+      //     body: JSON.stringify(this.comment),
+      //   },
+      // ).then((response) => {
+      //   response.json()
+      //   if (response.status === 201) {
+      //     //temporarily add the comment
+
+      //     if (this.comment.mail && this.comment.message) {
+      //       console.log(this.comment.mail)
+      //       console.log(this.comment.message)
+
+      //       this.comments.push(this.comment)
+      //     }
+      //     alert('Your comment has been added')
+      //     this.comments.push(this.comment)
+      //   }
+      // })
       // .then((response) => {
       //   alert(response)
       //   console.log(response)
@@ -173,22 +228,31 @@ export default {
   display: block;
   width: 100%;
   margin: 0.5rem auto;
+  padding: 0.5rem;
+  outline: none;
 }
 
 label.form__el {
   text-align: left;
 }
 
+input.form__el {
+  height: 2rem;
+  border-radius: 5px;
+  border: solid 1px #ccc;
+}
+
 textarea.form__el {
   border-radius: 5px;
   outline: none;
-  transition: ease-in 200ms;
-  padding: 0.3rem;
+  transition: ease-in-out 200ms;
 }
 
+input:active,
+input:hover,
 textarea.form__el:hover,
 textarea.form__el:active {
-  box-shadow: 1px 1px 5px 2px #19bf692d;
+  box-shadow: 1px 1px 5px 2px #8f91902d;
 }
 
 button.form__el {
